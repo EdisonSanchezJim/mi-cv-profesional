cd5de6eedbd2c1a9df09843da5a1e4230fd7f6bc
EdisonSanchezJim <115053765+EdisonSanchezJim@users.noreply.github.com>
2026-02-22 13:12:43 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT; // ⚠️ IMPORTANTE
+
+app.use(express.static(__dirname));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
