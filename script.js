667e8d2b828faceb13266228d60ea0c261de74c4
EdisonSanchezJim <115053765+EdisonSanchezJim@users.noreply.github.com>
2026-02-22 12:03:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Animación al hacer scroll
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+document.querySelectorAll("section").forEach(section => {
+    section.classList.add("hidden");
+    observer.observe(section);
+});
