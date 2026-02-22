d58d58b21055e466f65f13a2a26d660e3221f5b2
EdisonSanchezJim <115053765+EdisonSanchezJim@users.noreply.github.com>
2026-02-22 14:44:20 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT; // ⚠️ IMPORTANTE
+const port = process.env.PORT;
 
-app.use(express.static(__dirname));
+// Servir archivos desde carpeta frontend
+app.use(express.static(path.join(__dirname, '../frontend')));
 
+// Redirigir todo a index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
 app.listen(port, () => {
